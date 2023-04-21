bbc5027055b32872f91bc93755bec5fcee9e6d59
Júlia Ancilotto <juliancilotto@gmail.com>
2023-04-20 22:02:59 -0300

    form using chakra components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useState } from "react";
+import { Input, Stack } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
 
 function App() {
+  const [name, setName] = useState(""); //para manter o estado dos valores a medida que são digitados
+  const [lastName, setLastName] = useState("");
+
+  const [displayText, setDisplayText] = useState("");
+
+  //handleSubmit é chamada para exibir o alert
+  const handleSubmit = (e: any) => {
+    //preventDefault impede que a página seja recarregada após o envio
+    e.preventDefault();
+    setDisplayText(`Meu nome é ${name} e meu sobrenome é ${lastName}`);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Test</h1>
+      {/*chakra components*/}
+      <form className="form" onSubmit={handleSubmit}>
+        <>
+          <Stack spacing={10}>
+            <Input
+              placeholder="Whats your name?"
+              size="sm"
+              width="165px"
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+            />
+
+            <Input
+              placeholder="Whats your last name?"
+              size="sm"
+              width="165px"
+              value={lastName}
+              onChange={(event) => setLastName(event.target.value)}
+            />
+          </Stack>
+        </>
+
+        <>
+          <Stack>
+            <Button type="submit" colorScheme="teal" variant="solid">
+              Enviar
+            </Button>
+          </Stack>
+        </>
+      </form>
+
+      <div className="text">{displayText}</div>
     </div>
   );
 }
